279a66d3f53ded56d71d846a231b836d8691b060
Patryk <patryko@bydnet.pl>
2024-08-08 09:35:12 +0200

    Checkpoints for best models during training, metrics throught training saved to pkl file. Figures folder for printing figures to master papers.

---
+++ customLib/print_model_parameters.py
@@ -0,0 +1,15 @@
+from torch import nn
+def print_model_parameters(model):
+    total_params = 0
+    for name, layer in model.named_modules():
+        if name == "":
+            continue
+
+        if isinstance(layer, nn.Module) and len(list(layer.parameters())) > 0:
+            layer_params = sum(param.numel() for param in layer.parameters() if param.requires_grad)
+            statement = f"Layer {name}:" if name != "" else "Total params:"
+            print(f"{statement} {layer_params} parameters")
+            total_params += layer_params
+
+    print(f"Total trainable params: {total_params}")
+    
