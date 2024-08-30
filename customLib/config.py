a4c9cfef781d3e49cf932b7b1a1187c1777c29a4
Patryk <patryko@bydnet.pl>
2024-08-30 18:02:13 +0200

    Added fantasia path to config file

--- customLib/config.py
+++ customLib/config.py
@@ -11,3 +11,6 @@ qtdb_sampling_rate = 250
 
 lobachevsky = "E:\\ml-data\\masters-thesis\\physionet.org\\files\\lobachevsky-university-electrocardiography-database-1.0.1"
 lobachevsky_sampling_rate = 500
+
+fantasia_path = "E:\\ml-data\\masters-thesis\\physionet.org\\files\\fantasia-database-1.0.0"
+fantasia_sampling_rate=250
